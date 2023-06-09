aee115ca71f335f7315b1a9f3dcb7dea5b298e1c
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-09 09:56:04 -0700

    updated

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from sys import argv
+    
+    length = len(argv)
+    if length == 0:
+        print("{} arguments.".format(length))
+    else:
+        if length == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, argv[i]))
