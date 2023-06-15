eb1b839e835f1f70c570896047d508b0f1e9a628
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-15 06:35:47 -0700

    updated

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_num = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+    }
+
+    if not isinstance(roman_string, str) or len(roman_string) == 0:
+        return
+    else:
+        roman_int = 0
+        for key in roman_string:
+            if roman_int < roman_num[key]:
+                roman_int = roman_num[key] - roman_int
+            else:
+                roman_int += roman_num[key]
+        return roman_int
