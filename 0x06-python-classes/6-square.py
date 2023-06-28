bfc68a0be0ffba169d0d5542a93cead6e7621299
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-27 17:41:12 -0700

    updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,6 +11,7 @@ class Square:
 
         Args:
             size (int): The size of the new square.
+            position (tuple): The position of the square.
         """
         self.size = size
         self.position = position
@@ -22,6 +23,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -48,6 +50,7 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
+        """prints the square in #."""
         if (self.size == 0):
             print("")
         else:
