cf15faf6b78d7fdc908de3770aae9421b5ccb7ec
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-07-06 09:44:29 -0700

    updated

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a class rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
@@ -48,8 +48,8 @@ class Rectangle:
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2 + self.__height * 2)
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
@@ -58,16 +58,16 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
+
         rect = []
-        for col in range(self.__height):
-            row = "#" * self.__width
-            rect.append(row)
-            rect.append("\n")
-        rect.pop()
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
         return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle (" + str(self.__width) + ", "
-        rect += str(self.__height) + ")"
-        return rect
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
