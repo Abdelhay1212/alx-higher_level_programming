8cc2afb60bec9bc807798cf67d4b9a9dc0bcf2ff
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-09-11 10:42:58 -0700

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let arrOfSTRs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let str of arrOfSTRs) {
+const arrOfSTRs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const str of arrOfSTRs) {
   console.log(str);
 }
