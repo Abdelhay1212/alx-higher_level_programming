399518f6b9b98fa28467e01da079a745a80f9770
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-01 01:54:47 +0200

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)"""
+""" sends a request to the URL and displays
+    the body of the response (decoded in utf-8)."""
 
 
 import sys
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+# /usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("""Body response:
+\t- type: {}
+\t- content: {}""".format(type(str(content)), content.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
