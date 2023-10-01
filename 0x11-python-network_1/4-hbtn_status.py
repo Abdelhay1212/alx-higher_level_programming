7e36679a6b05390de5ceb60b397095474e78f5ec
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-01 02:03:08 +0200

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,11 +8,8 @@ import urllib.request
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("""Body response:
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("""Body response:
 \t- type: {}
 \t- content: {}""".format(type(str(content)), content.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
