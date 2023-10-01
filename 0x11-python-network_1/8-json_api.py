71deb573a7d3850e3eeac6060d65eafbb521aef6
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-01 03:13:18 +0200

    updated

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': "" if len(sys.argv) == 1 else sys.argv[1]}
+
+    r = requests.post(url, data)
+    try:
+        valid_json = r.json()
+        if valid_json == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+    except:
+        print("Not a valid JSON")
