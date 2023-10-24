6a4a206a21e7d4df9ff11c8824a7e11aea056fa5
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 22:12:14 +0200

    updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-/*script that prints the title of a Star
-Wars movie where the episode number matches a given integer.*/
+/*script that prints the title of a Star Wars movie where the episode number matches a given integer.*/
 
 const request = require('request');
 
-ID = process.argv[2];
-URL = `https://swapi-api.alx-tools.com/api/films/${ID}`;
+const ID = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${ID}`;
 
 request.get(URL, (error, response, body) => {
   console.log(error || JSON.parse(body).title);
