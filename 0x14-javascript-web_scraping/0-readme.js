d4fb843c945da9ed6ece659e2ae443e62657f0fe
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 19:28:50 +0200

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 const args = process.argv;
 
 fs.readFile(args[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
