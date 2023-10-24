97712d61b6d4261aba7da02c9c07e26ab0748444
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 14:49:37 -0700

    updated

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,11 +8,11 @@ const URL = process.argv[2];
 const pathFile = process.argv[3];
 
 request.get(URL, (error, response, body) => {
-    if (!error) {
-        fs.writeFile(pathFile, body, 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    }
+  if (!error) {
+    fs.writeFile(pathFile, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
