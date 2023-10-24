5d73f26bace69c453c405b5825d2255c723fc89c
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 19:39:04 +0200

    updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,6 +8,6 @@ const content = process.argv[3];
 
 fs.writeFile(pathFile, content, 'utf8', (err) => {
   if (err) {
-    console.error('Error writing the file:', err);
+    console.error(err);
   }
 });
