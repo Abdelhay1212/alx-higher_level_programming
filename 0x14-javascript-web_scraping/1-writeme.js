657a72589faf1feb9ddabe6b4c0e479f3017b39c
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 19:38:17 +0200

    updated

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* script that writes a string to a file */
+
+const fs = require('fs');
+
+const pathFile = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(pathFile, content, 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing the file:', err);
+  }
+});
