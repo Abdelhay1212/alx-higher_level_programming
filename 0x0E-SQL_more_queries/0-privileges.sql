76d0e218665596edd4b0fcf85e3c087610739db0
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-12-21 08:08:45 +0100

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,1 +1,2 @@
-# script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SELECT * FROM mysql.user WHERE User LIKE 'user_0d_%';
